f4bc44eecf6e074edad8e4efbd5e7eb148461ba9
Eugene <eugeneberegovoj@gmail.com>
2016-10-14 14:57:34 +0300

    Add link CSS on layout

--- ExpressApp3/app.js
+++ ExpressApp3/app.js
@@ -1,14 +1,17 @@
 ﻿var http = require('http');
 var express = require('express');
+var path = require('path');
 var app = express();
 var index = require('./routes/index');
 var bodyParser = require('body-parser');
 
 
-app.set('views', './views');
+//app.set('views', './views');
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 //var app = new express();
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(bodyParser.urlencoded({
     extended: true

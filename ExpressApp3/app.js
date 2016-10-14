8881c4cfbc39966d8275c53b990452e9355c6423
Eugene <eugeneberegovoj@gmail.com>
2016-10-14 14:19:24 +0300

    Add project files.

---
+++ ExpressApp3/app.js
@@ -0,0 +1,38 @@
+﻿var http = require('http');
+var express = require('express');
+var app = express();
+var index = require('./routes/index');
+var bodyParser = require('body-parser');
+
+
+app.set('views', './views');
+app.set('view engine', 'jade');
+
+//var app = new express();
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.use(bodyParser.json());
+
+
+
+app.get('/', function (req, res) {
+    res.send('Lets start');
+});
+
+app.use('/index', index);
+
+
+
+//app.listen(3000, function () {
+//       console.log('example app listening on port 3000!');
+//}); 
+
+app.server = http.createServer(app).listen(3000, function () {
+    console.log("Server running...");
+});
+
+
+//module.exports = app;
---
+++ ExpressApp3/routes/index.js
@@ -0,0 +1,19 @@
+﻿var express = require('express');
+var router = express.Router();
+
+
+/* GET home page. */
+router.get('/', function (req, res) {
+    res.render('index', { title: 'Express' });
+    
+}); 
+
+router.post('/', function (req, res) {    
+    res.send("Post Page");
+    console.log(req.body.user.name);
+    console.log(req.body.user.description);
+    
+    
+});
+
+module.exports = router;

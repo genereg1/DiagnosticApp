8cb5c544afcd77e7b6456f4006ef57ca31dba07d
Eugene <eugeneberegovoj@gmail.com>
2016-10-17 09:58:24 +0300

    Render Post Page

--- ExpressApp3/routes/index.js
+++ ExpressApp3/routes/index.js
@@ -9,10 +9,18 @@ router.get('/', function (req, res) {
 }); 
 
 router.post('/', function (req, res) {    
-    res.send("Post Page");
+    var pacientName = req.body.user.name;
+    var pacientAge = req.body.user.age;
+    var pacientDescription = req.body.user.description; 
+    var pacientSex = req.body.gender;
+    //res.send("Post Page");
+    res.render('post', { pacientName, pacientAge, pacientDescription, pacientSex });
+
     console.log(req.body.user.name);
     console.log(req.body.user.age);
     console.log(req.body.user.description);
+    
+    
     
     
     

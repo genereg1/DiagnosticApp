333b4e8e32f4ac5ac3f7640f37352d8c799f3958
Eugene <eugeneberegovoj@gmail.com>
2016-11-28 20:29:50 +0200

    Connect to user-data collection

--- ExpressApp3/routes/index.js
+++ ExpressApp3/routes/index.js
@@ -10,9 +10,16 @@ var diseaseDateSchema = new Schema({
     diagnostic: String
 }, {collection: 'disease-data'});
 
+var userDateSchema = new Schema({
+    name: String,
+    gender: String,
+    age: Number,
+    description: String
+}, {collection: 'user-data'});
 
 
 var DiseaseDate = mongoose.model('DiseaseDate', diseaseDateSchema); 
+var UserDate = mongoose.model('UserDate', userDateSchema);
 
 /* GET home page. */
 
@@ -28,23 +35,22 @@ router.get('/get', function(req, res, next) {
    });   
 });
 
-//test route
-router.get('/find', function(req, res, next) {
-    DiseaseDate.find({symptoms: "testS"}).then(function(doc) {
-        res.render('index', {items: doc});
-    });
-});
 
-router.post('/get', function (req, res) {    
-    var pacientName = req.body.user.name;
-    var pacientGender = req.body.gender;
-    var pacientAge = req.body.user.age;
-    var pacientDescription = req.body.user.description; 
-    var regExp = new RegExp(pacientDescription, 'i');
+router.post('/get', function (req, res) { 
+    var item = {
+        name: req.body.user.name,
+        gender: req.body.gender,
+        age: req.body.user.age,
+        description: req.body.user.description
+    };
+
+    var pacientDescription = req.body.user.description;
+    var regExp = new RegExp(item.description, 'i');
     
     
-    //res.send("Post Page");
-    //res.render('post', { pacientName, pacientAge, pacientDescription, pacientGender });
+    var data = new UserDate(item);
+    data.save();
+
     DiseaseDate.find({symptoms: regExp}).then(function(doc) {
         
         res.render('index', { items: doc });
